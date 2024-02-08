13bb496307981410d55a22677e9baeca6d5df5a5
sidharth <sidharth4network@gmail.com>
2024-02-08 09:02:59 +0530

    WAP to print the pattern as follows if input is 5:

--- classworks.sh
+++ classworks.sh
@@ -1,16 +1,58 @@
-checkIfMultiple() {
-    if [ $2 -eq 0 ]; then
-    echo "error: cannot divide by 0"
-    return 1
-    fi
-    if (( $1%$2 == 0 )); then
-    echo "$1 is a multiple of $2"
-    return 0
-    else 
-    echo "$1 is not a multiple of $2"
-    return 1
-    fi
+
+#!/usr/bin/bash
+
+printInversePyramid() {
+declare -i num
+ printf "enter a number "
+ read num
+    for ((i=$num ; i>=1 ; i--)); do
+        for ((j=i ; j>=1 ; j--)); do
+            printf "*"
+        done
+    printf "\n"
+    done
 }
 
-checkIfMultiple 11 5
-echo $?
+printInversePramidMirrored() {
+    declare -i num
+    printf "enter a number"
+    read num
+        for ((i=$num; i>=1; i--)); do
+        whiteSpace=$(($num-$i))
+            for ((j=0; j<$whiteSpace; j++)); do
+            printf " "
+            done
+            for ((k=$i; k>=1; k--)); do
+            printf "*"
+            done
+        printf "\n"
+        done
+}
+
+printOneZeroTriangle(){
+    declare -i num
+    printf "enter a number "
+    read num
+    declare -i flag=1
+    for ((i=1; i<=$num; i++)); do
+        whiteSpace=$(($num-$i))
+        for ((j=0; j<$whiteSpace; j++)); do
+            printf " "
+        done
+        for ((k=0; k<$i; k++)); do
+            if (($flag==1)); then
+                printf "%d" 1
+                flag=0
+            else
+                printf "%d" 0
+                flag=1
+            fi
+        done
+        printf "\n"
+        flag=1
+    done
+}
+
+printInversePyramid
+printInversePramidMirrored
+printOneZeroTriangle
