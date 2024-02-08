fa0875e4b00ee4f7b9efdc9d0ac50d293f773b64
sidharth <sidharth4network@gmail.com>
2024-02-08 09:43:51 +0530

    WAP to check whether a number is palindrome or not.

--- classworks.sh
+++ classworks.sh
@@ -1,58 +1,19 @@
-
 #!/usr/bin/bash
-
-printInversePyramid() {
-declare -i num
- printf "enter a number "
- read num
-    for ((i=$num ; i>=1 ; i--)); do
-        for ((j=i ; j>=1 ; j--)); do
-            printf "*"
-        done
-    printf "\n"
-    done
-}
-
-printInversePramidMirrored() {
+palindromeCheck() {
     declare -i num
-    printf "enter a number"
+    echo "enter a number: "
     read num
-        for ((i=$num; i>=1; i--)); do
-        whiteSpace=$(($num-$i))
-            for ((j=0; j<$whiteSpace; j++)); do
-            printf " "
-            done
-            for ((k=$i; k>=1; k--)); do
-            printf "*"
-            done
-        printf "\n"
-        done
-}
-
-printOneZeroTriangle(){
-    declare -i num
-    printf "enter a number "
-    read num
-    declare -i flag=1
-    for ((i=1; i<=$num; i++)); do
-        whiteSpace=$(($num-$i))
-        for ((j=0; j<$whiteSpace; j++)); do
-            printf " "
-        done
-        for ((k=0; k<$i; k++)); do
-            if (($flag==1)); then
-                printf "%d" 1
-                flag=0
-            else
-                printf "%d" 0
-                flag=1
-            fi
-        done
-        printf "\n"
-        flag=1
+    result=0
+    numDupe=$num
+    while (( $numDupe!=0 )); do
+        onesPlace=$(($numDupe%10))
+        result=$(($result*10+$onesPlace))
+        numDupe=$(($numDupe/10))
     done
+    if (($result==$num)); then
+        echo "it is a palindrome"
+    else
+        echo "it is not a palindrome"
+    fi
 }
-
-printInversePyramid
-printInversePramidMirrored
-printOneZeroTriangle
+palindromeCheck
